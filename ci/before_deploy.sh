e2a26dd2e7b22490f070506388d5bf1433ca4c5f
dejankos <kosdejan@yahoo.com>
2021-05-27 21:14:34 +0200

    release flag fix again

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,7 +17,7 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    cross rustc --bin imet --target $TARGET --release -- -C lto
+    cross rustc --bin imet --target $TARGET --release
 
     cp target/$TARGET/release/imet $stage/
 
